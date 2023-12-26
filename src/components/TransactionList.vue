d31691475c848f1b03d2b4c116ed1569e21d0267
unknown <edilson.lopes097@outlook.com>
2023-12-26 17:23:39 -0100

    Added the transation list, balance and income expenses components

--- src/components/TransactionList.vue
+++ src/components/TransactionList.vue
@@ -1,31 +1,21 @@
 <template>
   <h3>History</h3>
   <ul id="list" class="list">
-    <li v-for="transaction in transactions" 
-    :key="transaction.id" 
-    :class="transaction.amount < 0 ? 'minus' : 'plus'">
+    <li v-for="transaction in transactions" :key="transaction.id" :class="transaction.amount < 0 ? 'minus' : 'plus'">
 
       {{ transaction.text }} <span>${{ transaction.amount }}</span><button class="delete-btn">x</button>
-      
+
     </li>
   </ul>
 </template>
 
-<script>
+<script setup>
+  import { defineProps } from 'vue';
 
-export default {
-  setup(){
-  const transactions = [
-        { id: 1, text: 'Flower', amount: -19.99 },
-        { id: 2, text: 'Salary', amount: 299.97 },
-        { id: 3, text: 'Book', amount: -10 },
-        { id: 4, text: 'Camera', amount: 150 },
-      ];
-
-      return {
-        transactions,
-      }
- }
-}
-
+  const props = defineProps({
+    transactions: {
+      type: Array,
+      required: true,
+    }
+  });
 </script>
