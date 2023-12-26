bd4b182d419caacc3285054a0cd14680d7cae36e
unknown <edilson.lopes097@outlook.com>
2023-12-26 15:57:33 -0100

    Create base components for app tracker

---
+++ src/components/TransactionList.vue
@@ -0,0 +1,12 @@
+<template>
+    <h3>History</h3>
+    <ul id="list" class="list">
+        <li class="plus">
+          Payment <span>$500</span><button class="delete-btn">x</button>
+        </li>
+        <li class="minus">
+          Cash <span>-$400</span><button class="delete-btn">x</button>
+        </li>
+
+    </ul>
+</template>
